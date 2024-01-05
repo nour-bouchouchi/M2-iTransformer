21aceabaa96a3a20890ca900bed845ef150a50e8
nour-bouchouchi <55494306+nour-bouchouchi@users.noreply.github.com>
2024-01-05 18:50:08 +0100

    Add files via upload

--- dataloader.py
+++ dataloader.py
@@ -5,8 +5,7 @@ import torch
 from torch.utils.data import Dataset
 
 class MonDataLoaderAllData(Dataset):
-    def __init__(self, data, lookback_size, lookforward_size, scaler=None):
-        
+    def __init__(self, data, lookback_size, lookforward_size, scaler=None):     
         self.lookback_size = lookback_size
         self.lookforward_size = lookforward_size
         self.data = data
@@ -18,14 +17,14 @@ class MonDataLoaderAllData(Dataset):
             self.scaler = None
 
         d = []
-        for i in range(0, 1 + len(data) - (self.lookback_size+ self.lookforward_size), 1):
+        for i in range(0, 1 + len(data) - (self.lookback_size + self.lookforward_size), 1):
             seq_x = self.data[i:i+self.lookback_size, :]
             seq_y = self.data[i+self.lookback_size:i+self.lookback_size+self.lookforward_size, :]
             d.append((seq_x, seq_y))
         self.data = d
 
     def __len__(self):
-        return len(self.data) - self.lookback_size - self.lookforward_size + 1
+        return len(self.data) -1 #- self.lookback_size - self.lookforward_size + 1
 
     def __getitem__(self, idx):
       seq_x, seq_y = self.data[idx]
@@ -35,6 +34,7 @@ class MonDataLoaderAllData(Dataset):
 
 def get_loaders(data, batch_size, n_train, n_eval, n_test, T=96, S=96):
     train_data = data[ : n_train+T+S]
+    print("train_data ", train_data.shape)
     val_data = data[n_train : n_train+n_eval+T+S]
     test_data = data[n_train+n_eval : n_train+n_eval+n_test+T+S]
 
